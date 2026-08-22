5a676ced280cc04bd01a2122341ad83860d4f30b
Pichaya Puttekulangkura <micky47561@gmail.com>
2026-08-22 13:00:51 +0700

    :recycle: share the backdrop accent math out of @zframes/unicorn

--- packages/unicorn/src/index.ts
+++ packages/unicorn/src/index.ts
@@ -1,2 +1,8 @@
 export { default as UnicornScene } from "./scene";
 export { useLowEndDevice, useReducedMotion } from "./device";
+export {
+  ACCENT_DEFAULT_HUE,
+  ACCENT_DEFAULT_SAT,
+  accentRotation,
+  accentSaturation,
+} from "./accent";
